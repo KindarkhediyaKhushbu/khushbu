50d9dbc416e9e0cde3448db52868a8d8a3086927
khushbu kindarkhediya <khushbukindarkhediya@gmail.com>
2024-10-04 13:20:13 +0530

    new  line add

---
+++ some.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+void main()
+{
+    printf("hello world");
+    int x=5;
+    printf("%d print invalue",x);
+    var =k_shi;
+    printf("%d print in value",var);
+    
+    printf("%d print",khushi);
+
+}
