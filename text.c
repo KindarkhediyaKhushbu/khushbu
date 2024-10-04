f7131723a13be6fd823c6b5f21aab7b4b5e714e7
KindarkhediyaKhushbu <khushbukindarkhediya@gmail.com>
2024-10-04 14:40:08 +0530

    Add files via upload

---
+++ text.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+
+void main()
+{
+    int variable;
+
+    variable=20;
+    printf("%d",variable);
+
+    int num=10;
+    printf("\n %d before update--");
+    printf("\n %d",num);
+
+    num=20;
+    printf("\n after upadte");
+    printf("%d",num);
+
+
+
+    int a1=19;
+    char may_name= "k";
+    int into=10;
+
+
+   // camelCase
+    int isUserLoggedInWithEmailId;
+
+    // PascalCase
+    int IsUserLoggedInWithEmailId;
+
+    // kabab-case
+    // int is-user-logged-in-with-email-id
+
+    // snake_case
+    int is_user_logged_in_with_email_id;
+
+
+
+}
